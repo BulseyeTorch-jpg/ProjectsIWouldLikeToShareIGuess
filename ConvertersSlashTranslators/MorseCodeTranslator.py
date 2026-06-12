d285170d6e38880df418d2c1b62ea997c92ed511
TargetTorch <thisissomeone2011@gmail.com>
2026-06-12 10:46:20 +0100

    So now it does something

--- ConvertersSlashTranslators/MorseCodeTranslator.py
+++ ConvertersSlashTranslators/MorseCodeTranslator.py
@@ -1,18 +1,18 @@
 AlphaList = ["a","b","c","d","e","f","g",
 	"h","i","j","k","l","m","n","o","p",
 	"q","r","s","t","u","v",
-	"w","x","y","z"]
+	"w","x","y","z"] #Maybe add more
 
 MorseList = [".-","-...","-.-.","-..",".","..-.","--.",
 	"....","..",".---","-.-",".-..","--","-.","---",".--.",
 	"--.-",".-.","...","-","..-","...-",
-	".--","-..-","-.--","--.."]
+	".--","-..-","-.--","--.."] #Maybe add more (Found an 'International' morse list)
 
 def theListEning(A): 
 	listA = []
 	for i in range(len(A)): 
 		listA.append(A[i])
-		return listA
+	return listA
 def Mfilter(B): 
 	Contact = bool(False)
 	Filtered = []
@@ -21,11 +21,22 @@ def Mfilter(B):
 			case "."|"-" if Contact == False: 
 				Filtered.append(B[i])
 				Contact = True
+				Added = B[i]
 			case "."|"-" if Contact == True: 
-				Filtered.append(Filtered.pop(-1)+B[i])
+				Filtered.pop(-1)
+				Filtered.append(Added+B[i])
+				Added = Filtered[-1]
 				Contact = True
-			case " "|","|"/"|"\\": #2 \s = 1 \ cuz escaping or something idk
+			case " "|",":
 				Contact = False
+			case"/": 
+				Contact = False
+				Filtered.append(" ")
 			case _:
 				Contact = False
 	return Filtered
+Morseified = str(input("Insert the morse code sentence you would like to translate:"))
+print(Morseified)
+Peeled = theListEning(Morseified)
+Baked = Mfilter(Peeled)
+print(Baked)
